8e1072ac778c62fc71314e3ed96ad30165792f23
khushisharma2004 <ks969250@gmail.com>
2026-06-24 00:11:28 +0530

    khushi new enhanced ui

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { LandingPage } from '../pages/LandingPage';
+import { StudentDashboard } from '../pages/StudentDashboard';
 
 export const AppRoutes: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-<<<<<<< HEAD
-=======
         <Route path="/StudentDashboard" element={<StudentDashboard />} />
->>>>>>> d300c93 (khushboo)
       </Routes>
     </BrowserRouter>
   );
